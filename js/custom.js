06444e687d0ebe93545ca0f0db5d3b48c4411b3c
Akshath Sivaprasad <akshath@berkeley.edu>
2016-04-07 04:37:10 -0700

    progress on blog

--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,23 @@ $('.nav-tabs a').click(function (e) {
 	$('html,body').scrollTop(scrollmem);
 });
 
+[].forEach.call(document.querySelectorAll('code'), function($code) {
+    var lines = $code.textContent.split('\n');
+
+    if (lines[0] === '')
+    {
+        lines.shift()
+    }
+
+    var matches;
+    var indentation = (matches = /^[\s\t]+/.exec(lines[0])) !== null ? matches[0] : null;
+    if (!!indentation) {
+        lines = lines.map(function(line) {
+            line = line.replace(indentation, '')
+            return line.replace(/\t/g, '    ')
+        });
+
+        $code.textContent = lines.join('\n').trim();
+    }
+});
+
